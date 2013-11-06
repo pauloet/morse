d4fc151374372f1731074f9a0e80cbd3cf50fbd1
Paulo Simões <paulo.lopes.simoes@gmail.com>
2013-11-06 17:22:23 +0100

    3 communication model working good (distance, line_of_sight, free_space_loss)

--- My_Simulations/comm_testing_main.py
+++ My_Simulations/comm_testing_main.py
@@ -1,26 +1,30 @@
 #! /usr/bin/env python3.2
 import sys
 import communications_simulator as comm_simu
+from time import sleep
+
 
 def main():
 
-    r0r1 = comm_simu.Communication_Simulator('r00', 'r11')
-    r0r1.set_model('abc')
-    r1r2 = comm_simu.Communication_Simulator('r11', 'r22')
-    r1r2.set_model('line_of_sight')
 
-
-    r2r3 = comm_simu.Communication_Simulator('r22', 'r33', model = "empiric_1")
-    print(r2r3.get_model())
-    r2r3.set_model('line_of_sight')
-    print(r2r3.get_model())
-    r2r3 = comm_simu.Communication_Simulator('r22', 'r33')
-    print(r2r3.get_model())
-    r2r3 = comm_simu.Communication_Simulator('r22', 'r33', model = "non_existance")
-    print(r2r3.get_model())
+    r1r2_1 = comm_simu.Communication_Simulator('r1', 'r2', model = "distance")
+    r1r2_2 = comm_simu.Communication_Simulator('r1', 'r2', model = "line_of_sight")
+    r1r2_3 = comm_simu.Communication_Simulator('r1', 'r2', model = "free_space_loss", freq = 750)
     
-    r2r3 = comm_simu.Communication_Simulator('r22', 'r33', mod = "non_existance")
-    print(r2r3.get_model())
+    try:
+        while True:
+            print("Distance Model: %s" %(r1r2_1.can_communicate()))    
+            print("Line of Sight Model: %s" %(r1r2_2.can_communicate()))
+            print("Propagation Path Loss Model: %s" %(r1r2_3.can_communicate()))
+            sleep(7)    # seconds
+    except (KeyboardInterrupt, SystemExit):
+        print(' --> Programm terminated.')
+        raise     
+    finally: 
+        del r1r2_1
+        del r1r2_2
+        del r1r2_3
+        pass
 
 if __name__ == "__main__":
     main()
--- My_Simulations/communications_simulator.py
+++ My_Simulations/communications_simulator.py
@@ -1,4 +1,5 @@
-from pymorse import Morse
+import pymorse
+import math
 import logging
 
 # -----------------------------LOGGING CONFIGURATION -----------------------------------
@@ -26,42 +27,115 @@ class Communication_Simulator():
 
     __models = ('distance', 'line_of_sight', 'free_space_loss', 'empiric_1')
     __default_model = 'distance'
+    __default_distance_threshold = 10   # In Morse: 1 unit --> 1 meter
+    __default_frequency = 800           # MHz
+    __default_path_loss_threshold = 115 # dB
 
     def __init__(self, robot_1, robot_2, **kwargs):
         """ Initialization / Constructor
-        Robot names are assumed to be correct! It can also be settled the communication model in this call.
+        Robot names are assumed to be correct! Here, the communication model (and specifications) can be settled.
+        Default values are settled, in case the respective arguments are not passed. 
+        Kwown arguments: 'model', 'distance_threshold', 'freq' and 'pathloss_threshold'
+        Available Models: 'distance', 'line_of_sight', 'free_space_loss'
         Usage examples:
-            r0r1 = communications_simulator.Communication_Simulator('r00', 'r11') 
-            r0r1 = communications_simulator.Communication_Simulator('r00', 'r11', model = "free_space_loss") 
-            
-            In case the model argument doesn't exist, the default one is settled."""
-        
+            r0r1 = communications_simulator.Communication_Simulator('r0', 'r1') 
+            r0r1 = communications_simulator.Communication_Simulator('r0', 'r1', model = "free_space_loss")                 
+            r0r1 = communications_simulator.Communication_Simulator('r0', 'r1', model = "free_space_loss", freq = 800)
+            r0r1 = communications_simulator.Communication_Simulator('r0', 'r1', model = "free_space_loss", pathloss_threshold = 120)
+            ...
+            r0r1 = communications_simulator.Communication_Simulator('r0', 'r1', \
+                    model = "free_space_loss", freq = 800, pathloss_threshold = 120)
+            r0r1 = communications_simulator.Communication_Simulator('r0', 'r1', model = "distance", pathloss_threshold = 100)
+
+            Of course, the last example doesn't make sense because the model 'distance' is not related with the loss threshold.
+            So, this method has to be used consciously.
+            Distance Threshold --> In Morse: 1 unit = 1 meter
+            Frequency --> MHz
+            Path Loss Threshold --> dB """
+
+        self.morse = pymorse.Morse()
         self.__r1 = robot_1
         self.__r2 = robot_2
-        if "model" in kwargs:
-            self.set_model(kwargs["model"])
-        else:
-            self.set_model(self.__default_model)
+        self.__model = None
+        self.__distance_threshold = None
+        self.__frequency = None
+        self.__pathloss_threshold = None
+        self.set_model_specifications(**kwargs)
+        
     
+    def __del__(self):
+        """ Destructor """
+        self.morse.close()
+        logger.info('Morse socket closed')        
+
 
     def can_communicate(self):
         """ This public method returns 'True' or 'False' if the 2 robots can communicate or not. """
-        return True
-
-
-    def set_model(self, model):
-        """ This public method set a communication model between the 2 robots.
-        If the 'model' argument doesn't exist, it sets to the default one. 
-        A log(debug) message is given in the last case. """
-        
-        if self.model_exists(model):
-            self.__model = model
+                
+        if self.__model == 'distance':
+            return self.__simulate_comm_distance()
+        elif self.__model == 'line_of_sight':
+            return self.__simulate_comm_lineofsight()
+        elif self.__model == 'free_space_loss':
+            return self.__simulate_comm_freespaceloss()
         else:
-            logger.warning("Communication model '%s' doesn't exist."%model.upper())
-            self.__set_model2default()
+            logger.debug("An error occurred in the 'Class -> Communication_Simulator.can_communicate() method.")
+            return False
+
+
+    def set_model_specifications(self, **kwargs):
+        """ This public method set a communication model and its specifications between the 2 robots.
+            Kwown arguments: 'model', 'freq' and 'pathloss_threshold'
+            Usage examples:\
+                    r0r1.set_model_specifications(model = "free_space_loss", freq = 800, pathloss_threshold = 120))
+                    r0r1.set_model_specifications(freq = 500)
+                    ...
+                    r0r1.set_model_specifications(model = "distance", pathloss_threshold = 100)
+
+            Of course, the last example doesn't make sense because the model 'distance' is not related with the loss threshold.
+            So, this method has to be used consciously.                
+            Distance Threshold --> In Morse: 1 unit = 1 meter
+            Frequency --> MHz
+            Path Loss Threshold --> dB """
         
+        if ("model" in kwargs) and self.model_exists(kwargs["model"]):
+            self.__model = kwargs["model"]
+        else:
+            if self.__model is None:
+                self.__model = self.__default_model
+            #else:  Keeping the previous one
+
         logger.info("Communication model '%s' was settled between '%s' and '%s'."\
-                    %(self.__model.upper(), self.__r1.upper(), self.__r2.upper()))
+                %(self.__model.upper(), self.__r1.upper(), self.__r2.upper()))
+        #---------------------------------------
+
+        if "distance_threshold" in kwargs:
+            self.__distance_threshold = kwargs["distance_threshold"]
+        else:
+            if self.__distance_threshold is None:
+                self.__distance_threshold = self.__default_distance_threshold
+            #else: Keeping the previous value
+        
+        logger.info("Distance Threshold (m): %i" %self.__distance_threshold)
+        #---------------------------------------
+
+        if "freq" in kwargs:
+            self.__frequency = kwargs["freq"]
+        else:
+            if self.__frequency is None:
+                self.__frequency = self.__default_frequency
+            #else: Keeping the previous value
+
+        logger.info("Frequency (MHz): %i" %self.__frequency)
+        #---------------------------------------
+        
+        if "pathloss_threshold" in kwargs:
+            self.__pathloss_threshold = kwargs["pathloss_threshold"]
+        else:
+            if self.__pathloss_threshold is None:
+                self.__pathloss_threshold = self.__default_path_loss_threshold
+        
+        logger.info("Path Loss Threshold (dB): %s" %self.__pathloss_threshold)
 
 
     def model_exists(self, model):
@@ -69,23 +143,77 @@ class Communication_Simulator():
         if model.lower() in self.__models:  return True
         else:                               return False
 
-    
-    def __set_model2default(self):
-        """ This (supposed) private method sets the default communication model between the 2 robots. """
-        self.__model = self.__default_model
-
 
     def get_model(self):
         """ This public method returns the current communication model between the 2 robots. """
         return self.__model
     
     
+    def __simulate_comm_distance(self):
+        """ If the distance between the 2 robots is greater than DISTANCE THRESHOLD, they can't communicate (return False). """
+
+        result = self.__get_distance_and_lineofsight()
+        if result[0] < self.__distance_threshold: 
+            logger.info("Communications Simulator: Robots '%s' and '%s' can communicate\
+                    because DISTANCE is: %i (<%i)"%(self.__r1.upper(), self.__r2.upper(), result[0], self.__distance_threshold))
+            return True
+            
+        else:  
+            logger.info("Communications Simulator: Robots '%s' and '%s' CANNOT communicate\
+                    because DISTANCE is: %i (>=%i)"%(self.__r1.upper(), self.__r2.upper(), result[0], self.__distance_threshold))
+            return False
+
+
+    def __simulate_comm_lineofsight(self):
+        """ If the 2 robots can see each other, they can communicate (return True). """
+        
+        result = self.__get_distance_and_lineofsight()
+        if result[1]:
+            logger.info("Communications Simulator: Robots '%s' and '%s' can communicate\
+                    because LINE-of-SIGHT is: %s"%(self.__r1.upper(), self.__r2.upper(), result[1]))
+        else:            
+            logger.info("Communications Simulator: Robots '%s' and '%s' CANNOT communicate\
+                    because LINE-of-SIGHT is: %s"%(self.__r1.upper(), self.__r2.upper(), result[1]))
+        return result[1]
+
+
+    def __simulate_comm_freespaceloss(self):
+        """ If the path loss is greater than PATHLOSS THRESHOLD, they can't communicate (return False)
+        Formula obtained from: 'Propagation Path Loss Models for Mobile Communication', Klozar and Prokopec, IEEE 2011
+        This formula doesn't consider effects of propagation in real environment, i. e., with no distortion. """
+        # TODO: When the antennas will be defined on Morse, then get the gain Gt, Gr and apply the generic formula (paper 2002)
+
+
+        # frequency in MHz:
+        result = self.__get_distance_and_lineofsight()
+        loss = 32.44 + 20*math.log(self.__frequency, 10) + 20*math.log(result[0], 10)
+
+        if loss < self.__pathloss_threshold: 
+            
+            logger.info("Communications Simulator: Robots '%s' and '%s' can communicate\
+                    because PATH LOSS is: %i (<%i)"%(self.__r1.upper(), self.__r2.upper(), loss, self.__pathloss_threshold))
+            return True
+            
+        else:  
+            logger.info("Communications Simulator: Robots '%s' and '%s' CANNOT communicate\
+                    because PATH LOSS is: %i (>=%i)"%(self.__r1.upper(), self.__r2.upper(), loss, self.__pathloss_threshold))
+            return False
+
+    def __get_distance_and_lineofsight(self):
+        """ Return a list with 2 arguments: distance, (boolean) line of sight. """
+
+        try:
+                return self.morse.rpc('communication', 'distance_and_view', self.__r1, self.__r2)
+        
+        except  pymorse.MorseServerError as mse:
+            print('Oups! An error occurred!')
+            print(mse)
+
     def print_models(self):
         # This method was only for debugging...
         print("\n---------------------------------\nAvailable models:\n\t")
         print("\n".join(self.__models))
         print("\n---------------------------------\n")
-
 
 
 
